6b69de4d826235f4ed5ecad7b2d1e68636493444
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2022-03-26 11:33:48 +0000

    feat: main.rs

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::os;
+
+use actix_web::{get, App, HttpServer, Responder};
+use std::env;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let podname = match env::var("POD_NAME") {
+        Ok(val) => val,
+        Err(err) => err.to_string(),
+    };
+    format!("The name of the pod on which the application is running is {}", podname)
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("0.0.0.0", 80))?
+        .run()
+        .await
+}
