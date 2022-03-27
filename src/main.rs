8ddd03f9e183a83633b8d336eba022d5f9fd4fd2
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2022-03-27 10:28:23 +0000

    feat: Add response header

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, App, HttpServer, HttpResponseBuilder, http::{StatusCode, header::ContentType}, HttpResponse};
 use std::env;
 
 #[get("/")]
-async fn index() -> impl Responder {
+async fn index() -> HttpResponse {
     let podname = match env::var("POD_NAME") {
         Ok(val) => val,
         Err(err) => err.to_string(),
     };
-    format!("{}", podname)
+    HttpResponseBuilder::new(StatusCode::ACCEPTED)
+        .append_header(ContentType::plaintext())
+        .append_header(("Access-Control-Allow-Origin", "*"))
+        .body(podname)       
 }
 
 #[actix_web::main]
